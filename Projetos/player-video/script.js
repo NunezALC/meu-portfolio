fc5651d3cfff5067a65fff3c219a719687ff9084
NunezALC <ndorgivaldo@gmail.com>
2026-04-02 01:53:45 -0300

    Adicionando botão de mudo e atalhos

--- Projetos/player-video/script.js
+++ Projetos/player-video/script.js
@@ -2,6 +2,7 @@ const video = document.getElementById('video-principal');
 const btnPlay = document.getElementById('btn-play-pause');
 const barraProgresso = document.getElementById('barra-progresso');
 const tempoTexto = document.getElementById('tempo-atual');
+const btnMute = document.getElementById('btn-mute');
 
 btnPlay.addEventListener('click',() => {
     if (video.paused) {
@@ -26,4 +27,30 @@ video.addEventListener('timeupdate', () => {
 barraProgresso.addEventListener('input', () => {
     const tempo = (barraProgresso.value / 100) * video.duration;
     video.currentTime = tempo;
+});
+
+btnMute.addEventListener('click', () => {
+    // A propriedade .muted é um booleano (true ou false)
+    video.muted = !video.muted; 
+    
+    // Feedback visual para o usuário
+    if (video.muted) {
+        btnMute.innerText = "Desmutar";
+        btnMute.classList.replace('btn-outline-secondary', 'btn-danger');
+    } else {
+        btnMute.innerText = "Mudo";
+        btnMute.classList.replace('btn-danger', 'btn-outline-secondary');
+    }
+});
+
+document.addEventListener('keydown', (event) => {
+    // Se apertar Espaço, dá Play/Pause
+    if (event.code === 'Space') {
+        event.preventDefault(); // Impede que a página desça ao apertar espaço
+        btnPlay.click(); // Simula o clique no botão que você já criou
+    }
+    // Se apertar 'M', silencia
+    if (event.key.toLowerCase() === 'm') {
+        btnMute.click();
+    }
 });
